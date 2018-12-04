32d70dc33c69046c36cc4feecc6d9d3111be1ddc
John Coombs <johnc@backbase.com>
2018-12-04 10:31:07 -0500

    add custom input text component

--- fxcm-local-project/apps/fxcm-app/src/app/app.customClassId.ts
+++ fxcm-local-project/apps/fxcm-app/src/app/app.customClassId.ts
@@ -5,8 +5,15 @@ export function customClassId(name: string, properties: FormItemProperties): str
   console.log('++ properties', properties);
   
   // Add your FXCM custom properties here. 
-  if (properties.classId == 'BbFormButtonComponent') {
-    properties.classId = 'FxcmFormButtonComponent';
+  if (properties.classId === 'BbFormButtonComponent') {
+    return 'FxcmFormButtonComponent';
+  }
+  
+  if (properties.classId === 'BbFormInputTextComponent') {
+    const props: FormItemField = properties as FormItemField;
+    if (props.dataType === 'text') { // password || email !== 'text'
+      return 'FxcmFormInputTextComponent';
+    }
   }
  
   return properties.classId;  // Always return the existing classId if you are not changing it
