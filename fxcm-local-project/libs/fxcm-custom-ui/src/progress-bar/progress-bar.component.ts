2562055a140e5a974fb0b998f2b2f02e356972c0
John Coombs <johnc@backbase.com>
2018-12-11 15:24:56 -0500

    added progress-bar component

--- fxcm-local-project/libs/fxcm-custom-ui/src/progress-bar/progress-bar.component.ts
+++ fxcm-local-project/libs/fxcm-custom-ui/src/progress-bar/progress-bar.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'bb-progress-bar-ui',
   templateUrl: './progress-bar.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.onPush
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent implements OnInit, OnChanges {
+  @Input() complete: number = 0;
+  
+  constructor() { 
+  }
 
-  constructor() { }
-
-  ngOnInit() { }
+  ngOnInit() {
+  } 
 }
