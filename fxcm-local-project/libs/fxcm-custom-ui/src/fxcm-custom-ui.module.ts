303b0b868540a3fbe6207698bcd3663dfa5e8149
John Coombs <johnc@backbase.com>
2018-12-11 13:49:08 -0500

    created forms library

--- fxcm-local-project/libs/fxcm-custom-ui/src/fxcm-custom-ui.module.ts
+++ fxcm-local-project/libs/fxcm-custom-ui/src/fxcm-custom-ui.module.ts
@@ -1,36 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackbaseCoreModule } from '@backbase/core-ang';
-import { BbFormsErrorsModule } from '@backbase/forms-errors-ang';
-
-// Directives
-import { BbFormsDirectivesModule } from '@backbase/forms-directives-ang';
-
-// Components
-import { FxcmFormButtonComponent } from './components/fxcm-form-button/fxcm-form-button.component';
-import { FxcmFormInputTextComponent } from './components/fxcm-form-input-text/fxcm-form-input-text.component';
-import { FxcmFormInputCheckboxComponent } from './components/fxcm-form-input-checkbox/fxcm-form-input-checkbox.component';
-import { FxcmFormFlagSelectorComponent } from './components/fxcm-form-flag-selector/fxcm-form-flag-selector.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    BbFormsErrorsModule,
-    BbFormsDirectivesModule,
     BackbaseCoreModule.withConfig({
       classMap: {
-        FxcmFormButtonComponent,
-        FxcmFormInputTextComponent,
-        FxcmFormInputCheckboxComponent,
-        FxcmFormFlagSelectorComponent
       },
     }),
   ],
   declarations: [
-    FxcmFormButtonComponent,
-    FxcmFormInputTextComponent,
-    FxcmFormInputCheckboxComponent,
-    FxcmFormFlagSelectorComponent
   ]
 })
 export class FxcmCustomUiModule { }
